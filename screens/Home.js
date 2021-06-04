a31b81e73fa05c04e7ac9b301815d5f057ddfdd1
Mainul-Fahim <36801303+Mainul-Fahim@users.noreply.github.com>
2021-06-04 16:03:34 +0600

    first feature done

---
+++ screens/Home.js
@@ -0,0 +1,213 @@
+import React from 'react';
+import {
+    StyleSheet,
+    SafeAreaView,
+    View,
+    Text,
+    Image,
+    TouchableOpacity,
+    FlatList,
+    Modal
+} from 'react-native'
+import {
+    Svg,
+    Polygon
+} from 'react-native-svg';
+import { BlurView } from "expo-blur";
+
+import { images, icons, COLORS, FONTS, SIZES } from '../constants';
+
+const Home = ({ navigation }) => {
+
+    const [showAddToBagModal, setShowAddToBagModal] = React.useState(false)
+    const [selectedItem, setSelectedItem] = React.useState(null)
+    const [selectedSize, setSelectedSize] = React.useState("")
+
+    // Dummy Data
+    const [trending, setTrending] = React.useState([
+        {
+            id: 0,
+            name: "Nike Air Zoom Pegasus 36",
+            img: images.nikePegasus36,
+            bgColor: "#BF012C",
+            type: "RUNNING",
+            price: "$186",
+            sizes: [6, 7, 8, 9, 10]
+        },
+        {
+            id: 1,
+            name: "Nike Metcon 5",
+            img: images.nikeMetcon5Black,
+            bgColor: "#D39C67",
+            type: "TRAINING",
+            price: "$135",
+            sizes: [6, 7, 8, 9, 10, 11, 12]
+        },
+        {
+            id: 2,
+            name: "Nike Air Zoom Kobe 1 Proto",
+            img: images.nikeZoomKobe1Proto,
+            bgColor: "#7052A0",
+            type: "BASKETBALL",
+            price: "$199",
+            sizes: [6, 7, 8, 9]
+        },
+    ]);
+
+    const [recentlyViewed, setRecentlyViewed] = React.useState([
+        {
+            id: 0,
+            name: "Nike Metcon 4",
+            img: images.nikeMetcon4,
+            bgColor: "#414045",
+            type: "TRAINING",
+            price: "$119",
+            sizes: [6, 7, 8]
+        },
+        {
+            id: 1,
+            name: "Nike Metcon 6",
+            img: images.nikeMetcon6,
+            bgColor: "#4EABA6",
+            type: "TRAINING",
+            price: "$135",
+            sizes: [6, 7, 8, 9, 10, 11]
+        },
+        {
+            id: 2,
+            name: "Nike Metcon 5",
+            img: images.nikeMetcon5Purple,
+            bgColor: "#2B4660",
+            type: "TRAINING",
+            price: "$124",
+            sizes: [6, 7, 8, 9]
+        },
+        {
+            id: 3,
+            name: "Nike Metcon 3",
+            img: images.nikeMetcon3,
+            bgColor: "#A69285",
+            type: "TRAINING",
+            price: "$99",
+            sizes: [6, 7, 8, 9, 10, 11, 12, 13]
+        },
+        {
+            id: 4,
+            name: "Nike Metcon Free",
+            img: images.nikeMetconFree,
+            bgColor: "#A02E41",
+            type: "TRAINING",
+            price: "$108",
+            sizes: [6, 7, 8, 9, 10, 11]
+        },
+    ]);
+
+    // Render
+
+    function renderTrendingShoes(item, index) {
+        var trendingStyle = {};
+
+        if (index == 0) {
+            trendingStyle = { marginLeft: SIZES.padding, }
+        } else {
+            trendingStyle = {}
+        }
+
+        return (
+            <TouchableOpacity
+                style={{ height: 240, width: 180, justifyContent: 'center', marginHorizontal: SIZES.base, ...trendingStyle }}
+                onPress={() => {
+                    setSelectedItem(item)
+                    setShowAddToBagModal(true)
+                }}
+            >
+                <Text style={{ color: COLORS.gray, ...FONTS.h5 }}>{item.type}</Text>
+
+                <View style={[{
+                    flex: 1,
+                    justifyContent: 'flex-end',
+                    marginTop: SIZES.base,
+                    borderTopLeftRadius: 10,
+                    borderBottomLeftRadius: 10,
+                    borderBottomRightRadius: 10,
+                    marginRight: SIZES.padding,
+                    paddingLeft: SIZES.radius,
+                    paddingRight: SIZES.padding,
+                    paddingBottom: SIZES.radius,
+                    backgroundColor: item.bgColor
+                }, styles.trendingShadow]}>
+                    <View style={{ height: '35%', justifyContent: 'space-between' }}>
+                        <Text style={{ color: COLORS.white, ...FONTS.body4 }}>{item.name}</Text>
+                        <Text style={{ color: COLORS.white, ...FONTS.h3 }}>{item.price}</Text>
+                    </View>
+                </View>
+
+                <View style={{ position: 'absolute', top: 27, right: 0, width: "95%", height: "100%" }}>
+                    <Svg height="100%" width="100%">
+                        <Polygon
+                            points="0,0 160,0 160,80"
+                            fill="white"
+                        />
+                    </Svg>
+                </View>
+
+                <Image
+                    source={item.img}
+                    resizeMode="cover"
+                    style={{
+                        position: 'absolute',
+                        top: 50,
+                        right: 0,
+                        width: "98%",
+                        height: 80,
+                        transform: [
+                            { rotate: '-15deg' }
+                        ]
+                    }}
+                />
+            </TouchableOpacity>
+        )
+    }
+
+    
+
+    
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLORS.white
+    },
+    trendingShadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 5,
+        },
+        shadowOpacity: 0.29,
+        shadowRadius: 4.65,
+
+        elevation: 7,
+    },
+    recentContainerShadow: {
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 7,
+        },
+        shadowOpacity: 0.43,
+        shadowRadius: 9.51,
+
+        elevation: 15,
+    },
+    absolute: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0
+    }
+})
+
+export default Home;
